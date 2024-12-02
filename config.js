810abe26607186e7b6cf99f2906dcfaec08ea49e
pahansarhsara360 <Pahansathsara6345@gmail.com>
2024-12-02 12:25:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "7mhlUYzb#ABOoEZUjBdNIeJixaA2dvjL9h3qqtvTzRm_dayKl18A", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "n3wkTKib#_QkvGYrf9uMy8VDliLAUwdoSucIj4SFk-TZWTF87Z9I", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
-MODE: process.env.MODE || "only me",
+MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "true",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",    
 };
